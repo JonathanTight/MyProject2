8643daa0d488f774cb9dcf8ef7cf44e59a692449
kirillsafin <kirill.safin1991@gmail.com>
2026-05-26 16:33:03 +0300

    2605

---
+++ docs/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+
+
+int main() // Программы на C++ начинаются с выполнения функции main
+{
+    cout << "U name?:\n";
+    string name;
+    cin >> name;
+    cout << "Hello, " << name << "!\n";
+}
